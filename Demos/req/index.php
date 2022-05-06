e75fc1cd318ddde2fccaeec52c08a8b573d856c6
Duedot43 <jackgendill@icloud.com>
2022-05-06 15:58:30 -0600

    uloa

--- Demos/req/index.php
+++ Demos/req/index.php
@@ -4,11 +4,11 @@ $passwd = $_GET['passwd'];
 $hostname = $_GET['hostname'];
 $url = '?uname=' . $uname . '&passwd=' . $passwd . '&hostname=' . $hostname
 ?>
-<button onclick="/Demos/req/departed.php<?php echo $url;?>">View departed users</button>
-<button onclick="/Demos/req/view_user.php<?php echo $url;?>">View all users</button>
-<button onclick="/Demos/req/view_rooms.php<?php echo $url;?>">View all rooms</button>
-<button onclick="/Demos/req/remove_room.php<?php echo $url;?>">Remove a room</button>
-<button onclick="/Demos/req/remove_user.php<?php echo $url;?>">Remove a user</button>
-<button onclick="/Demos/req/mk_user.php<?php echo $url;?>">Make a user</button>
-<button onclick="/Demos/req/mk_room.php<?php echo $url;?>">Make a room</button>
-<button onclick="/Demos/req/edit_user.php<?php echo $url;?>">Edit a user</button>
+<input type="button" value="View departed users"onclick='/req/departed.php<?php echo $url;?>'\>
+<input type="button" value="Edit user"onclick='/req/edit_user.php<?php echo $url;?>'\>
+<input type="button" value="Edit user"onclick='/req/edit_user.php<?php echo $url;?>'\>
+<input type="button" value="Edit user"onclick='/req/edit_user.php<?php echo $url;?>'\>
+<input type="button" value="Edit user"onclick='/req/edit_user.php<?php echo $url;?>'\>
+<input type="button" value="Edit user"onclick='/req/edit_user.php<?php echo $url;?>'\>
+<input type="button" value="Edit user"onclick='/req/edit_user.php<?php echo $url;?>'\>
+<input type="button" value="Edit user"onclick='/req/edit_user.php<?php echo $url;?>'\>
