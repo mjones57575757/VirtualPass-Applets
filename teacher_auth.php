40285af34436235411d4257277aa19504d355a4f
Duedot43 <jackgendill@icloud.com>
2022-05-09 13:26:13 -0600

    Now it works

--- teacher_auth.php
+++ teacher_auth.php
@@ -1,8 +1,7 @@
 <?php
 function auth($uname, $passwd){
-    $json_ou = exec("curl -u " . $_POST['uname'] . ":" . $_POST['passwd'] . " 'https://riidp.cherrycreekschools.org/api/rest/profiles/aggregated/my'");
+    $json_ou = file_get_contents("https://" . $uname . ":" . $passwd . "@riidp.cherrycreekschools.org/api/rest/profiles/aggregated/my");
     $json_out = json_decode($json_ou, true);
-    print_r($status[1] . "\n");
     if (isset($json_out['httpStatusCode'])){
         return "Invalid username or password.";
     } else{
@@ -11,7 +10,7 @@ function auth($uname, $passwd){
         if ($status[1] == "Staff"){
             return true;
         } else{
-            return "You are not autherized to access this application";
+            return "you are not authorized to access this resource";
         }
     }
 
