543718b505163dca1ce4a21b265095e097f4e47e
Jack <75505449+Duedot43@users.noreply.github.com>
2022-05-10 13:03:16 -0600

    Update teacher_auth.php

--- Plugins/teacher_auth/teacher_auth.php
+++ Plugins/teacher_auth/teacher_auth.php
@@ -3,7 +3,7 @@ function auth($uname, $passwd){
     $context = stream_context_create(array(
     'http' => array('ignore_errors' => true),
     ));
-    $json_ou = file_get_contents("https://" . $uname . ":" . $passwd . "@my.cherrycreekschools.org/api/rest/profiles/aggregated/my" false, $context);
+    $json_ou = file_get_contents("https://" . $uname . ":" . $passwd . "@my.cherrycreekschools.org/api/rest/profiles/aggregated/my", false, $context);
     $json_out = json_decode($json_ou, true);
     if (isset($json_out['httpStatusCode'])){
         return "Invalid username or password.";
